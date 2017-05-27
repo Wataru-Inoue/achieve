30a5a4a8610dffa67809eaf2d0e5f37a77fedd5b
Wataru Inoue <wataru.inoue01@gmail.com>
2017-05-27 07:43:37 +0000

    add user_name

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,19 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-# 100.times do |n|
-#  email = Faker::Internet.email
-#  password = "password"
-#  User.create!(email: email,
-#               password: password,
-#               password_confirmation: password,
-#               )
-# end
+100.times do |n|
+  name = Faker::Name.name
+  email = Faker::Internet.email
+  password = "password"
+  User.create!(name: name,
+               email: email,
+               password: password,
+               password_confirmation: password,
+               )
+ end
 
 n = 1
-while n <= 103
+while n <= 100
  Blog.create(
    title: "タイトル",
    content: "ブログ",
