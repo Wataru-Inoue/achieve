eb675d7a675f78dfd24484002ce9af51cd36aa34
Wataru-Inoue <wataru.inoue01@gmail.com>
2017-09-16 20:37:26 +0900

    mail host change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'calm-tundra-50113.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'http://13.114.46.17/' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings =
   {
