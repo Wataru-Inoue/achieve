0216da8b017145b4bc9150a3b3225aa4b2a959e1
Wataru-Inoue <wataru.inoue01@gmail.com>
2017-09-16 23:12:58 +0900

    fog-aws

--- Gemfile
+++ Gemfile
@@ -106,3 +106,4 @@ end
 
   gem 'unicorn'
 
+  gem 'fog-aws'
--- config/initializers/carriewave.rb
+++ config/initializers/carriewave.rb
@@ -1,12 +1,18 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
       provider:              'AWS',
-      aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
-      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
       region:                'ap-northeast-1',
-      path_style:            true,
+      use_iam_profile: true,
   }
-
+=begin
+  config.fog_credentials = {
+    provider:              'AWS',
+    aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
+    aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+    region:                'ap-northeast-1',
+    path_style:            true,
+}
+=end
   config.fog_public     = true
   config.fog_attributes = {'Cache-Control' => 'public, max-age=86400'}
 
